c411750990454c8b768ffda2bbe33e0f7e3dfdfb
Andres Morales <Andresmorales.fuac@gmail.com>
2020-09-29 11:46:29 -0500

    Se crean las migraciones para la base de datos

---
+++ database/migrations/2020_09_29_162252_create_lots_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lots', function (Blueprint $table) {
+            $table->id();
+            $table->string('lot_number');
+            $table->foreignId('id_product');
+            $table->date('expiration_date');
+            $table->double('price', 9, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lots');
+    }
+}
--- database/migrations/2020_09_29_162742_create_sale_details_table.php
+++ database/migrations/2020_09_29_162742_create_sale_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSaleDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sale_details', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('id_sale');
+            $table->foreignId('id_lot');
+            $table->integer('quantity');
+            $table->double('price', 9, 2);
+            $table->boolean('state');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sale_details');
     }
 }
